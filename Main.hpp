2737b6157023c427fb8cbc23f523f733df3b3319
Marco FERNANDES <mafernan@e4r4p4.wethinkcode.co.za>
2018-07-08 12:12:49 +0200

    auto commit

--- Main.hpp
+++ Main.hpp
@@ -6,7 +6,7 @@
 /*   By: mafernan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/22 09:31:00 by mafernan          #+#    #+#             */
-/*   Updated: 2018/07/06 09:48:50 by mafernan         ###   ########.fr       */
+/*   Updated: 2018/07/08 10:43:47 by mafernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define MAIN_HPP
 
 # include "IOperand.hpp"
+# include "Factory.hpp"
 # include "Parse.hpp"
 
 #endif
