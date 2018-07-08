c96e29300867f0eed26a493574188f217c106a3c
Marco FERNANDES <mafernan@e4r19p3.wethinkcode.co.za>
2018-07-08 14:21:50 +0200

    removed Factory class and Operand class from file into a seperate file

--- IOperand.hpp
+++ IOperand.hpp
@@ -6,7 +6,7 @@
 /*   By: mafernan   <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/25 10/38/23 by mafernan          #+#    #+#             */
-/*   Updated: 2018/07/08 10:47:43 by mafernan         ###   ########.fr       */
+/*   Updated: 2018/07/08 13:56:42 by mafernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 
 # include <iostream>
 # include <string>
-# include "Factory.hpp"
+
+class Factory;
 
 enum eOperandType {
 	int8,
@@ -39,57 +40,6 @@ class IOperand {
 		virtual std::string const & toString( void ) const = 0; //Str representation of the instance
 
 		virtual ~IOperand( void ) {};
-};
-
-template<typename T>
-class Operand : public IOperand {
-	public:
-		std::string			_val;
-		eOperandType		_type;
-
-		Operand(std::string val, eOperandType type) : _val(val), _type(type) {}; // sets val and type
-		Operand( Operand const & src ) { *this = src; } // copy constructor
-		virtual ~Operand( void ) {}; // deconstructor
-		virtual eOperandType getType( void ) const { // get Type function
-			return (_type);
-		}
-		virtual int getPrecision( void ) const { //Precision of the type of the instance
-			switch (_type) {
-				case int8:
-					return (0);
-				case int16:
-					return (1);
-				case int32:
-					return (2);
-				case FLOAT:
-					return (3);
-				case DOUBLE:
-					return (4);
-			}
-		}
-		virtual std::string const & toString( void ) const { // return a string instance of value
-			return (_val);
-		}
-		Operand & operator=(Operand const & src) { // equal operator
-			if (this != &src) {
-				*this = src;
-			}
-			return (*this);
-		}
-		virtual IOperand const * operator+( IOperand const & rhs ) const { //Sum
-			long double		lhs_val = std::stold(this->_val);
-			long double		rhs_val = std::stold(rhs.toString());
-			long double		result = lhs_val + rhs_val;
-			Factory			f;
-			int		lhs_prec = this->getPrecision();
-			int		rhs_prec = rhs.getPrecision();
-			IOperand const * op = NULL;
-			if (lhs_prec < rhs_prec)
-				op = f.createOperand( rhs.getType() ,std::to_string(result));
-			else
-				op = f.createOperand( this->getType() ,std::to_string(result));
-			return (op);
-		}
 };
 
 #endif
