05517e38bbf546bc235399b2899a23a111f59166
Marco FERNANDES <mafernan@e4r4p4.wethinkcode.co.za>
2018-07-09 15:21:57 +0200

    auto commit

--- Commands.hpp
+++ Commands.hpp
@@ -6,7 +6,7 @@
 /*   By: mafernan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 09:11:39 by mafernan          #+#    #+#             */
-/*   Updated: 2018/07/09 10:01:24 by mafernan         ###   ########.fr       */
+/*   Updated: 2018/07/09 10:20:47 by mafernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@
 # include "Parse.hpp"
 
 void	push(std::string cmds[], std::vector<IOperand const *> & stack);
+void	run_reg_cmds(std::string cmd, std::vector<IOperand const *> & stack);
 
 #endif
