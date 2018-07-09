36be186f6147342c26cd560291936daa2aef497f
Marco FERNANDES <mafernan@e4r4p4.wethinkcode.co.za>
2018-07-09 16:14:30 +0200

    auto commit

--- Operand.cpp
+++ Operand.cpp
@@ -6,18 +6,22 @@
 /*   By: mafernan   <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/08 13/38/57 by mafernan          #+#    #+#             */
-/*   Updated: 2018/07/09 15:06:35 by mafernan         ###   ########.fr       */
+/*   Updated: 2018/07/09 15:47:58 by mafernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Operand.hpp"
 #include "Factory.hpp"
+#include <iomanip>
+
 
 // Constructor
 template<typename T>
 Operand<T>::Operand(std::string val, eOperandType type) : _type(type) {
+	std::cout << "type : " << type << std::endl; 
 	long double temp = std::stold(val);
 	T new_val = static_cast<T>(temp);
+	std::cout << std::setprecision(2) << "value : " << _val << std::endl; 
 	_val = std::to_string(new_val);
 };
 // copy
@@ -70,9 +74,11 @@ IOperand const & Operand<T>::operator=( IOperand const & rhs) {
 // plus operator
 template<typename T>
 IOperand const * Operand<T>::operator+( IOperand const & rhs ) const { //Sum
+	std::cout << "this->_val : " << this->_val << std::endl;
 	long double		lhs_val = std::stold(this->_val);
 	long double		rhs_val = std::stold(rhs.toString());
 	long double		result = lhs_val + rhs_val;
+	std::cout << "result : lhs : rhs\n" << result << " : " <<  lhs_val << " : " << rhs_val << std::endl;
 	int				lhs_prec = this->getPrecision();
 	int				rhs_prec = rhs.getPrecision();
 
