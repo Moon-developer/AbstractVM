f6704dc03ab89bb8eac8ba93abea1798e05f933a
Marco FERNANDES <mafernan@e5r1p7.wethinkcode.co.za>
2018-07-11 13:44:27 +0200

    commented out file parsing, fixed loop for extra functionality

--- Main.cpp
+++ Main.cpp
@@ -6,83 +6,83 @@
 /*   By: mafernan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/22 09:28:43 by mafernan          #+#    #+#             */
-/*   Updated: 2018/07/09 15:14:22 by mafernan         ###   ########.fr       */
+/*   Updated: 2018/07/11 13:25:03 by mafernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Main.hpp"
 
 // return true if exit cmd found for file
-bool	find_exit(std::string file)
-{
-	std::string		line;
-	std::ifstream	input;
-	bool			exit = false;
-
-	try
-	{
-		input.open(file);
-		while (std::getline(input, line))
-			if (remove_comment(line) == 0)
-				exit = true;
-		input.close();
-		if (exit == true)
-			return (true);
-		else
-			throw Error::SyntaxError();
-	}
-	catch (std::exception & e) 
-	{
-		std::cout << e.what() << std::endl;
-		return (false);
-	}
-}
+//bool	find_exit(std::string file)
+//{
+//	std::string		line;
+//	std::ifstream	input;
+//	bool			exit = false;
+//
+//	try
+//	{
+//		input.open(file);
+//		while (std::getline(input, line))
+//			if (remove_comment(line) == 0)
+//				exit = true;
+//		input.close();
+//		if (exit == true)
+//			return (true);
+//		else
+//			throw Error::SyntaxError();
+//	}
+//	catch (std::exception & e) 
+//	{
+//		std::cout << e.what() << std::endl;
+//		return (false);
+//	}
+//}
 
 // loop thru each line in the file 
 void	parse_file(std::string file)
 {
 	std::ifstream	input;
-	std::string		line;
-	bool			exit = false;
+//	std::string		line;
+//	bool			exit = false;
 
 	input.open(file);
-	if (find_exit(file) == false)
-	{
-		std::cout << "No exit command found in "+file << std::endl;
-		return ;
-	}
-	while (std::getline( input, line ) && exit != true)
-	{
-		std::string	cmds[4];
-		try
-		{
-			validate(line, cmds);
-			if (cmds[3] == "end") {
-				exit = true;
-			}
-			else if (cmds[3] == "reg_cmd")
-				std::cout << "run push command here" << std::endl;
-				//run_reg_cmds(cmds[0], stack);
-			else if (cmds[3] == "push/assert")
-			{
-				if (cmds[0] == "push") {
-					std::cout << "run push command here" << std::endl;
-					//push(cmds, stack);
-				}
-				else if (cmds[0] == "assert") {
-					std::cout << "run assert command here" << std::endl;
+//	if (find_exit(file) == false)
+//	{
+//		std::cout << "No exit command found in "+file << std::endl;
+//		return ;
+//	}
+//	while (std::getline( input, line ) && exit != true)
+//	{
+//		std::string	cmds[4];
+//		try
+//		{
+//			validate(line, cmds);
+//			if (cmds[3] == "end") {
+//				exit = true;
+//			}
+//			else if (cmds[3] == "reg_cmd")
+//				std::cout << "run push command here" << std::endl;
+//				//run_reg_cmds(cmds[0], stack);
+//			else if (cmds[3] == "push/assert")
+//			{
+//				if (cmds[0] == "push") {
+//					std::cout << "run push command here" << std::endl;
+//					//push(cmds, stack);
+//				}
+//				else if (cmds[0] == "assert") {
+//					std::cout << "run assert command here" << std::endl;
 //					assert(cmds, stack); // checks if the instance at the top of 
 //												the stack is of the same type and value
-				}
-			}
-			else if (cmds[3] == "end")
-				std::cout << line << std::endl;
-		}
-		catch (std::exception & e) 
-		{
-			std::cout << e.what() << std::endl;
-		}
-	}
+//				}
+//			}
+//			else if (cmds[3] == "end")
+//				std::cout << line << std::endl;
+//		}
+//		catch (std::exception & e) 
+//		{
+//			std::cout << e.what() << std::endl;
+//		}
+//	}
 	input.close();
 }
 
@@ -107,6 +107,7 @@ void	loop(void)
 	std::string				input;
 	std::string				test;
 	std::vector<IOperand const *>	stack;
+	int						line = 0;
 
 	while (!exit) 
 	{
@@ -114,18 +115,19 @@ void	loop(void)
 		try
 		{
 			std::string		cmds[4];
-			validate(input, cmds);
+			line++;
+			validate(line, input, cmds);
 			if (cmds[3] == "end") {
 				for (int i = stack.size() - 1; i >= 0; i--)
 					delete stack[i];
 				exit = true;
 			}
 			if (cmds[3] == "reg_cmd")
-				run_reg_cmds(cmds[0], stack);
+				run_reg_cmds(cmds, stack, line);
 			if (cmds[3] == "push/assert")
 			{
 				if (cmds[0] == "push") {
-					push(cmds, stack);
+					push(line, cmds, stack, input);
 				}
 				else if (cmds[0] == "assert") {
 					std::cout << "run assert command here" << std::endl;
