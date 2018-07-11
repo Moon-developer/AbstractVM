e8eb73587afe5b50ad437fef72a3d4b3306ddb2a
Marco FERNANDES <mafernan@e5r1p7.wethinkcode.co.za>
2018-07-11 13:43:15 +0200

    add new method to some error handling to print out line and command that has an error issue

--- Error.hpp
+++ Error.hpp
@@ -6,7 +6,7 @@
 /*   By: mafernan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/22 15:05:15 by mafernan          #+#    #+#             */
-/*   Updated: 2018/07/09 07:28:46 by mafernan         ###   ########.fr       */
+/*   Updated: 2018/07/11 13:29:53 by mafernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,32 @@ class Error {
 	public:
 		class SyntaxError : public std::exception {
 			public:
+				std::string		e_msg;
+				SyntaxError(const std::string& line, const std::string& input) {
+					e_msg = std::string("Syntax ERROR on line ["+line+"] "+input);
+				}
 				virtual const char* what() const throw() {
-						return "incorrect syntax given. Please type correct inputs.";
+						return e_msg.c_str();
 				}
 		};
 		class OverflowError : public std::exception {
 			public:
+				std::string		e_msg;
+				OverflowError(const std::string& line, const std::string& input) {
+					e_msg = std::string("Overflow ERROR on line ["+line+"] "+input);
+				}
 				virtual const char* what() const throw() {
-						return "Overflow error occured.";
+						return e_msg.c_str();
+				}
+		};
+		class UnderflowError : public std::exception {
+			public:
+				std::string		e_msg;
+				UnderflowError(const std::string& line, const std::string& input) {
+					e_msg = std::string("Underflow ERROR on line ["+line+"] "+input);
+				}
+				virtual const char* what() const throw() {
+						return e_msg.c_str();
 				}
 		};
 		class InstructionError : public std::exception {
@@ -34,28 +52,22 @@ class Error {
 						return "Unknown Instruction given.";
 				}
 		};
-		class UnderflowError : public std::exception {
-			public:
-				virtual const char* what() const throw() {
-						return "Underflow error occured.";
-				}
-		};
 		class EmptyError : public std::exception {
 			public:
 				virtual const char* what() const throw() {
-						return "Instruction pop on an emtpy stack.";
+						return "Stack size ERROR: Attempted operand on empty stack.";
 				}
 		};
 		class ZeroError : public std::exception {
 			public:
 				virtual const char* what() const throw() {
-						return "Tried to mod or divide by '0'.";
+						return "Operator ERROR: Can't mod/div by zero!";
 				}
 		};
 		class ExitError : public std::exception {
 			public:
 				virtual const char* what() const throw() {
-						return "No exit instruction found.";
+						return "File Format ERROR: No exit command found!";
 				}
 		};
 		class AssertError : public std::exception {
@@ -67,7 +79,17 @@ class Error {
 		class StackSizeError : public std::exception {
 			public:
 				virtual const char* what() const throw() {
-						return "Stack containes less than two values.";
+						return "Stack size less than two to use command!";
+				}
+		};
+		class NotInt8 : public std::exception {
+			public:
+				std::string		e_msg;
+				 NotInt8(const std::string& line, const std::string& input) {
+				 	e_msg = std::string("ASCII Convertion ERROR on line ["+line+"] "+input);
+				 }
+				virtual const char* what() const throw() {
+						return e_msg.c_str();
 				}
 		};
 		virtual const char* what() const throw() { 
