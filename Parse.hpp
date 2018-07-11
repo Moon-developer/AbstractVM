43b53679f4dce9014b17f5b624566d187b528813
Marco FERNANDES <mafernan@e5r1p7.wethinkcode.co.za>
2018-07-11 15:48:47 +0200

    auto commit

--- Parse.hpp
+++ Parse.hpp
@@ -6,13 +6,12 @@
 /*   By: mafernan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/02 14:58:51 by mafernan          #+#    #+#             */
-/*   Updated: 2018/07/11 13:24:35 by mafernan         ###   ########.fr       */
+/*   Updated: 2018/07/11 15:43:11 by mafernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSE_HPP
 # define PARSE_HPP
-
 
 # include <limits.h>
 # include <cfloat>
@@ -22,11 +21,13 @@
 # include <sstream>
 # include <regex>
 # include "Error.hpp"
+# include "IOperand.hpp"
 
 std::vector<std::string>	splitspace(std::string input);
 
 int		remove_comment(std::string & input);
 void	validate(int line, std::string & input, std::string cmds[]);
 void	CheckOUFlow(int line, std::string cmds[4], std::string input);
+void	CheckOverUnderFlow(eOperandType _type, long double size);
 
 #endif
