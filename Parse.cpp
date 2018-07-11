da1722ba6fa749590548971bd0bc61a3cc0f142c
Marco FERNANDES <mafernan@e5r1p7.wethinkcode.co.za>
2018-07-11 13:46:10 +0200

    changed exception throw to take in arguments

--- Parse.cpp
+++ Parse.cpp
@@ -6,7 +6,7 @@
 /*   By: mafernan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/02 14:50:39 by mafernan          #+#    #+#             */
-/*   Updated: 2018/07/09 10:00:40 by mafernan         ###   ########.fr       */
+/*   Updated: 2018/07/11 13:40:39 by mafernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ int		remove_comment(std::string & input)
 }
 
 // check if input options are true
-void	validate(std::string & input, std::string cmds[4]) 
+void	validate(int line, std::string & input, std::string cmds[4]) 
 {
 	std::regex		reg("(push|assert) (int8|int16|int32|float|double)\\s?\\(-?[[:digit:]]+(.?[[:digit:]]+)?\\)\\B");
 	std::regex		reg_cmds("add\\b|sub\\b|mod\\b|dump\\b|div\\b|mul\\b|pop\\b|print\\b");
@@ -75,7 +75,7 @@ void	validate(std::string & input, std::string cmds[4])
 		if (std::regex_search(input, value_match, digits) == true)
 			val = (value_match[0].str()).substr(1, (value_match[0].str()).size() - 2);
 		else
-			throw Error::SyntaxError();
+			throw Error::SyntaxError(std::to_string(line), input);
 		cmds[0] = base_match[1].str();
 		cmds[1] = base_match[2].str();
 		cmds[2] = val;
@@ -85,44 +85,46 @@ void	validate(std::string & input, std::string cmds[4])
 	else if (input.size() == 0)
 		return ;
 	else
-		throw Error::SyntaxError();
+		throw Error::SyntaxError(std::to_string(line), input);
 	return;
 }
 
 // check if function has overflow or underflow
-void	CheckOUFlow(std::string func, std::string num)
+void	CheckOUFlow(int line, std::string cmds[4], std::string input)
 {
+	std::string			num = cmds[2];
+	std::string			func = cmds[1];
 	long double size = std::stold(num);
 
 	if (func == "int8") {
 		if (size > INT8_MAX) {
-			throw Error::OverflowError();
+			throw Error::OverflowError(std::to_string(line), input);
 		}
 		if (size < INT8_MIN)
-			throw Error::UnderflowError();
+			throw Error::UnderflowError(std::to_string(line), input);
 	}
 	else if (func == "int16") {
 		if (size > INT16_MAX)
-			throw Error::OverflowError();
+			throw Error::OverflowError(std::to_string(line), input);
 		if (size < INT16_MIN)
-			throw Error::UnderflowError();
+			throw Error::UnderflowError(std::to_string(line), input);
 	}
 	else if (func == "int32") {
 		if (size > INT32_MAX)
-			throw Error::OverflowError();
+			throw Error::OverflowError(std::to_string(line), input);
 		if (size < INT32_MIN)
-			throw Error::UnderflowError();
+			throw Error::UnderflowError(std::to_string(line), input);
 	}
 	else if (func == "float") {
 		if (size > FLT_MAX)
-			throw Error::OverflowError();
+			throw Error::OverflowError(std::to_string(line), input);
 		if (std::fabsl(size) < FLT_MIN && std::fabsl(size) > 0)
-			throw Error::UnderflowError();
+			throw Error::UnderflowError(std::to_string(line), input);
 	}
 	else if (func == "double") {
 		if (size > DBL_MAX)
-			throw Error::OverflowError();
+			throw Error::OverflowError(std::to_string(line), input);
 		if (std::fabsl(size) < DBL_MIN && std::fabsl(size) > 0)
-			throw Error::UnderflowError();
+			throw Error::UnderflowError(std::to_string(line), input);
 	}
 }
