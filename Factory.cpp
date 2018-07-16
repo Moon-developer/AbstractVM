ec223203372c9914bc9857bbb630125b4f71a13d
Marco FERNANDES <mafernan@e4r4p4.wethinkcode.co.za>
2018-07-16 07:45:55 +0200

    auto commit

--- Factory.cpp
+++ Factory.cpp
@@ -6,15 +6,17 @@
 /*   By: mafernan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/12 14:04:48 by mafernan          #+#    #+#             */
-/*   Updated: 2018/07/12 16:26:15 by mafernan         ###   ########.fr       */
+/*   Updated: 2018/07/16 07:37:55 by mafernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Factory.hpp"
 #include "Operand.hpp"
 
+// Constructor
 Factory::Factory( void ) {};
 
+// Copy constructor
 Factory & Factory::operator=(Factory const & src) {
 	if (this != &src) {
 		*this = src;
@@ -22,25 +24,36 @@ Factory & Factory::operator=(Factory const & src) {
 	return (*this);
 }
 
+// Deconstructor
 Factory::~Factory( void ) {};
 
+// Creates an Object of type int8 
 IOperand const * Factory::createInt8(std::string const & value) const {
 	return (new Operand<int8_t>(value, int8));
 }
 
+// Creates an Object of type int16
 IOperand const * Factory::createInt16(std::string const & value) const {
 	return (new Operand<int16_t>(value, int16));
 }
 
+// Creates an Object of type int32 
 IOperand const * Factory::createInt32(std::string const & value) const {
 	return (new Operand<int32_t>(value, int32));
 }
+
+// Creates an Object of type float 
 IOperand const * Factory::createFloat(std::string const & value) const {
 	return (new Operand<float>(value, FLOAT));
 }
+
+// Creates an Object of type Double 
 IOperand const * Factory::createDouble(std::string const & value) const {
 	return (new Operand<double>(value, DOUBLE));
 }
+
+// An array of pointer functions are stored in a struct and returns the correct function
+// according to the given TYPE's value and then created with the correct VALUE string passed
 IOperand const * Factory::createOperand(eOperandType type, std::string const & value) const {
 	pt2Function PointerFuncArr [] {
 		&Factory::createInt8,
