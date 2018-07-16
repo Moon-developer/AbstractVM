7263239149afbd7d7ea1b906272a7884020b1e13
Marco FERNANDES <mafernan@e4r4p4.wethinkcode.co.za>
2018-07-16 07:45:59 +0200

    auto commit

--- IOperand.hpp
+++ IOperand.hpp
@@ -6,7 +6,7 @@
 /*   By: mafernan   <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/25 10/38/23 by mafernan          #+#    #+#             */
-/*   Updated: 2018/07/16 07:10:45 by mafernan         ###   ########.fr       */
+/*   Updated: 2018/07/16 07:45:13 by mafernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,15 @@ enum eOperandType {
 
 class IOperand {
 	public:
-		virtual int getPrecision( void ) const = 0; //Precision of the type of the instance
-		virtual eOperandType getType( void ) const = 0; //Type of the instance
-		virtual IOperand const * operator+( IOperand const & rhs ) const = 0; //Sum
-		virtual IOperand const * operator-( IOperand const & rhs ) const = 0; //Difference
-		virtual IOperand const * operator*( IOperand const & rhs ) const = 0; //Product
-		virtual IOperand const * operator/( IOperand const & rhs ) const = 0; //Quotient
-		virtual IOperand const * operator%( IOperand const & rhs ) const = 0; //Modulo
-		virtual std::string const & toString( void ) const = 0; //Str representation of the instance
-		virtual ~IOperand( void ) {};
+		virtual int getPrecision( void ) const = 0;								//Precision of the type of the instance
+		virtual eOperandType getType( void ) const = 0;							//Type of the instance
+		virtual IOperand const * operator+( IOperand const & rhs ) const = 0;	//Sum
+		virtual IOperand const * operator-( IOperand const & rhs ) const = 0;	//Difference
+		virtual IOperand const * operator*( IOperand const & rhs ) const = 0;	//Product
+		virtual IOperand const * operator/( IOperand const & rhs ) const = 0;	//Quotient
+		virtual IOperand const * operator%( IOperand const & rhs ) const = 0;	//Modulo
+		virtual std::string const & toString( void ) const = 0;					//Str representation of the instance
+		virtual ~IOperand( void ) {};											// deconstructor for compiler
 };
 
 #endif
