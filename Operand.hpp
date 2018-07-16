ac0df6fdeadcdff1fc2488be811b749fafa8251c
Marco FERNANDES <mafernan@e4r4p4.wethinkcode.co.za>
2018-07-16 07:12:50 +0200

    auto commit

--- Operand.hpp
+++ Operand.hpp
@@ -6,7 +6,7 @@
 /*   By: mafernan   <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/08 13/38/57 by mafernan          #+#    #+#             */
-/*   Updated: 2018/07/11 15:23:51 by mafernan         ###   ########.fr       */
+/*   Updated: 2018/07/16 07:11:00 by mafernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,21 @@
 template<typename T>
 class Operand : public IOperand {
 	public:
-		std::string			_val;
-		eOperandType		_type;
-
+		Operand( void );
 		Operand(std::string val, eOperandType type); 
 		Operand( Operand const & src );
 		virtual ~Operand( void ); 
+		std::string			_val;
+		eOperandType		_type;
 		virtual eOperandType getType( void ) const;
 		virtual int getPrecision( void ) const;
 		virtual std::string const & toString( void ) const;
-		IOperand const & operator=( IOperand const & rhs); 
 		virtual IOperand const * operator+( IOperand const & rhs ) const; 
 		virtual IOperand const * operator-( IOperand const & rhs ) const; 
 		virtual IOperand const * operator*( IOperand const & rhs ) const; 
 		virtual IOperand const * operator/( IOperand const & rhs ) const; 
 		virtual IOperand const * operator%( IOperand const & rhs ) const; 
+		IOperand const & operator=( IOperand const & rhs); 
 };
 
 #include "Operand.cpp"
